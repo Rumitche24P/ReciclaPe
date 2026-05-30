1d5abd7720327ddd13188c0990dda1ace5840f41
Rumitche24P <pacman8624@gmail.com>
2026-05-29 21:45:06 -0500

    Estructura inicial del proyecto ReciclaPe

---
+++ recojo-service/src/main/java/com/reciclape/recojo_service/RecojoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reciclape.recojo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecojoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecojoServiceApplication.class, args);
+	}
+
+}
